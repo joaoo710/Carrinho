dcf45919e2fcd8dc18d0f05d6211eeb83c353240
Adriel <adriel.fariasan@gmail.com>
2022-04-10 17:36:35 -0300

    Adiciona componente para mudar rotas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import App from './App';
 import { createRoot } from 'react-dom/client';
+import RouteSwitch from './RouteSwitch';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouteSwitch />
   </React.StrictMode>
 );
