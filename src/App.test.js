4a539986d4c55a458b063c6bf0510ff1fd1431e2
Adriel <adriel.fsantos@outlook.com>
2022-04-18 18:26:05 -0300

    Adiciona descricao para o teste de App

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Stinson Style/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Stinson Style/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
