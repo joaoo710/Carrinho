3be304adec8270d9a10bd99b3e7e3575ea0cf3ac
Adriel <adriel.fsantos@outlook.com>
2022-04-18 19:49:40 -0300

    Insere Header no App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import 'normalize.css';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
-  return <h1>Stinson Style</h1>;
+  return <Header />;
 }
 
 export default App;
