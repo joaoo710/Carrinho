88f735134b852583319119aa33ca023bab9fe55d
Adriel <adriel.fariasan@gmail.com>
2022-04-21 16:20:44 -0300

    Adiciona pagina Home

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Home from './pages/Home';
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />}>
+          <Route index element={<Home />} />
           <Route path='products' element={null} />
           <Route path='cart' element={null} />
         </Route>
