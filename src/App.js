72b6da0b85263409ac1b244a60976e65c1b452f4
Adriel <adriel.fariasan@gmail.com>
2022-04-21 15:27:30 -0300

    Adiciona state para contagem de itens no carrinho

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import 'normalize.css';
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 
 function App() {
+  const [cartCount, setCartCount] = useState(0);
   return (
     <>
-      <Header />
-      <Outlet />
+      <Header cartCount={cartCount} />
+      <Outlet setCartCount={setCartCount} />
     </>
   );
 }
