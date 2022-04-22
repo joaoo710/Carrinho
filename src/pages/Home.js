7e9105253fd819c557f14e3defa3432a1cedb1ca
Adriel <adriel.fariasan@gmail.com>
2022-04-21 22:04:19 -0300

    Insere um Hero na pagina Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import hero1 from '../img/hero/1.jpg';
+import Hero from '../components/Hero';
 
 const Home = (props) => {
-  return <section>HOME</section>;
+  return (
+    <section>
+      <Hero
+        title='Titulo um'
+        text='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero.'
+        heroCss={{
+          backgroundImage: `url(${hero1})`,
+          backgroundPositionX: 'center',
+          backgroundPositionY: 'center',
+          color: '#fff',
+        }}
+        gradientCss={{
+          background: 'linear-gradient(45deg, #6d8eadbb, rgba(0, 0, 0, 0))',
+        }}
+      />
+    </section>
+  );
 };
 
 export default Home;
