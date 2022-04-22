fab99c7c5cd47cea00e9d9bba82425d24a3c5be9
Adriel <adriel.fsantos@outlook.com>
2022-04-22 17:04:21 -0300

    Implementa selecao de alinhamento de conteudo

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,11 +2,11 @@ import Container from './Container';
 import '../css/Hero.css';
 
 const Hero = (props) => {
-  const { title, text, heroCss = {}, gradientCss = {} } = props;
+  const { title, text, heroCss = {}, bgCss = {}, posRight = false } = props;
   return (
     <div className='hero' style={heroCss}>
-      <div className='gradient' style={gradientCss}>
-        <Container>
+      <div className='gradient' style={bgCss}>
+        <Container classes={posRight ? ['right'] : ['left']}>
           {props.title !== '' ? <h2>{title}</h2> : null}
           {props.text !== '' ? <p>{text}</p> : null}
         </Container>
