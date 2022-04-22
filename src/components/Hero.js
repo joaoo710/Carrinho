39eb068a734ff513b617e57ab7c0df7c7b8fbdd8
Adriel <adriel.fariasan@gmail.com>
2022-04-21 22:03:57 -0300

    Melhora implementacao de Hero e adiciona seu css

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,14 @@
 import Container from './Container';
+import '../css/Hero.css';
 
 const Hero = (props) => {
+  const { title, text, heroCss = {}, gradientCss = {} } = props;
   return (
-    <div className='hero' style={{ backgroundImage: `url(${props.image})` }}>
-      <div className='gradient'>
+    <div className='hero' style={heroCss}>
+      <div className='gradient' style={gradientCss}>
         <Container>
-          {props.title !== '' ? <h2>{props.title}</h2> : null}
-          {props.text !== '' ? <p>{props.text}</p> : null}
+          {props.title !== '' ? <h2>{title}</h2> : null}
+          {props.text !== '' ? <p>{text}</p> : null}
         </Container>
       </div>
     </div>
