80504998dd1be26673bb8bc52024edc21ead4ef3
Adriel <adriel.fsantos@outlook.com>
2022-04-24 22:48:33 -0300

    Implementa tipo de visualizacao dos controles

--- src/components/ProductCardControls.js
+++ src/components/ProductCardControls.js
@@ -3,9 +3,10 @@ import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 import '../css/ProductCardControls.css';
 
 const ProductCardControls = (props) => {
-  const { addBtn, removeBtn, counter } = props;
+  const { addBtn, removeBtn, counter, full = false } = props;
+  const cssClasses = ['card-controls', full ? 'full' : null];
   return (
-    <div className='card-controls'>
+    <div className={cssClasses.join(' ')}>
       <button onClick={removeBtn}>
         <FontAwesomeIcon icon={faMinus} />
       </button>
