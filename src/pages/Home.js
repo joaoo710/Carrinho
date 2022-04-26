c925585cb264f10959e5d3bdfc8311521d70daf4
Adriel <adriel.fsantos@outlook.com>
2022-04-26 13:22:35 -0300

    Insere secao de produtos em destaque na Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import HomeCarousel from '../components/HomeCarousel';
+import ProductsHomeSection from '../components/ProductsHomeSection';
 
 const Home = (props) => {
+  const { addToCart } = props;
   return (
     <section>
       <HomeCarousel />
+      <ProductsHomeSection addToCart={addToCart} />
     </section>
   );
 };
