7986b8cf7deafb1d073bdb8f5314996ddcde0a61
Adriel <adriel.fsantos@outlook.com>
2022-04-27 23:31:06 -0300

    Adiciona css a pagina de produtos

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,15 +1,19 @@
+import Container from '../components/Container';
 import ProductCard from '../components/ProductCard';
 import products from '../data/products';
+import '../css/Products.css';
 
 const Products = () => {
   return (
-    <section>
-      <h2>Products</h2>
-      <div className='products-list'>
-        {products.map((product) => {
-          return <ProductCard product={product} key={product.id} />;
-        })}
-      </div>
+    <section id='products-page'>
+      <Container>
+        <h2>Products</h2>
+        <div className='products-list'>
+          {products.map((product) => {
+            return <ProductCard product={product} key={product.id} />;
+          })}
+        </div>
+      </Container>
     </section>
   );
 };
