f2eeb299609937d62f2a102c89d394d497d3939b
Adriel <adriel.fsantos@outlook.com>
2022-04-27 22:03:09 -0300

    Adiciona testes de Footer

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -1,4 +1,36 @@
 import { render, screen } from '@testing-library/react';
 import Footer from '../components/Footer';
 
-describe('Footer component', () => {});
+describe('Footer component', () => {
+  test('It renders the site name', () => {
+    render(<Footer />);
+    const siteTitle = screen.getByText(/Stinson Style/i);
+    expect(siteTitle).toBeInTheDocument();
+  });
+
+  test('It renders the site slogan', () => {
+    render(<Footer />);
+    const siteSlogan = screen.getByText(/Making you look awesome since 2005/i);
+    expect(siteSlogan).toBeInTheDocument();
+  });
+
+  test('It informs user that this site is just a template', () => {
+    render(<Footer />);
+    const siteInfo = screen.getByText(
+      /This is a fake store website developed with the purpose of practicing/i
+    );
+    expect(siteInfo).toBeInTheDocument();
+  });
+
+  test('It renders the site author name', () => {
+    render(<Footer />);
+    const siteAuthor = screen.getByText(/Adriel Faria/i);
+    expect(siteAuthor).toBeInTheDocument();
+  });
+
+  test('It renders two links', () => {
+    render(<Footer />);
+    const links = screen.getAllByRole('link');
+    expect(links).toHaveLength(2);
+  });
+});
