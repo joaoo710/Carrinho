0b9eeb17ba09c9409dc2f13e5470754114271cae
Adriel <adriel.fsantos@outlook.com>
2022-05-02 23:10:55 -0300

    Implementa pagina de carrinho

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,29 @@
+import CartInfoPanel from '../components/CartInfoPanel';
+import CartNoProductsMsg from '../components/CartNoProductsMsg';
+import CartProducts from '../components/CartProducts';
+import Container from '../components/Container';
+import '../css/Cart.css';
+
 const Cart = (props) => {
-  return <section id='cart'>Cart</section>;
+  const { cart, addToCart, removeFromCart } = props;
+  return (
+    <section id='cart'>
+      <Container>
+        <h2>Cart</h2>
+        {cart.length > 0 ? <CartInfoPanel cart={cart} /> : null}
+
+        {cart.length === 0 ? (
+          <CartNoProductsMsg />
+        ) : (
+          <CartProducts
+            cart={cart}
+            addToCart={addToCart}
+            removeFromCart={removeFromCart}
+          />
+        )}
+      </Container>
+    </section>
+  );
 };
 
 export default Cart;
