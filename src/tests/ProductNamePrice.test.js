d8ecd289fe6c72e5a0ec48c64d87b362b76679b7
Adriel <adriel.fsantos@outlook.com>
2022-05-08 22:14:06 -0300

    Adiciona testes sobre link de ProductNamePrice

--- src/tests/ProductNamePrice.test.js
+++ src/tests/ProductNamePrice.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import ProductNamePrice from '../components/ProductNamePrice';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('ProductNamePrice component', () => {
   test('It renders the name', () => {
@@ -30,5 +31,37 @@ describe('ProductNamePrice component', () => {
     render(<ProductNamePrice />);
     const price = screen.getByText(/0\.00/);
     expect(price).toBeInTheDocument();
+  });
+
+  test('It renders a link when hasLink is true', () => {
+    render(
+      <MemoryRouter>
+        <ProductNamePrice hasLink={true} />
+      </MemoryRouter>
+    );
+    const link = screen.getByRole('link');
+    expect(link).toBeInTheDocument();
+  });
+
+  test('If hasLink is false, no link is rendered', () => {
+    render(
+      <MemoryRouter>
+        <ProductNamePrice hasLink={false} link={'/products/1'} />
+      </MemoryRouter>
+    );
+    const links = screen.queryAllByRole('link');
+    expect(links).toHaveLength(0);
+  });
+
+  test('The link rendered matches the link passed', () => {
+    const link = '/products/1';
+    render(
+      <MemoryRouter>
+        <ProductNamePrice hasLink={true} link={link} />
+      </MemoryRouter>
+    );
+    const linkTag = screen.getByRole('link');
+    expect(linkTag).toBeInTheDocument();
+    expect(linkTag).toHaveAttribute('href', link);
   });
 });
